c3779e6de1967f51672b884517a89cd0d800f65d
Your Name <you@example.com>
2019-04-11 10:07:07 +0300

    second commit

--- app/src/main/java/com/example/earthquakeapp/EarthquakeActivity.java
+++ app/src/main/java/com/example/earthquakeapp/EarthquakeActivity.java
@@ -9,25 +9,31 @@ import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private EarthquakeAdapter eAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
 
-        ArrayList<String> earthquakes=new ArrayList<>();
-        earthquakes.add("San Francisco");
-        earthquakes.add("London");
-        earthquakes.add("Tokyo");
-        earthquakes.add("Mexico City");
-        earthquakes.add("Moscow");
-        earthquakes.add("Rio de Janeiro");
-        earthquakes.add("Paris");
+        listView = (ListView) findViewById(R.id.list);
 
-        ListView earthquakeListView=(ListView)findViewById(R.id.list);
+        ArrayList<Earthquake> earthquakes=new ArrayList<>();
+        earthquakes.add(new Earthquake("2.3","London","13/4/2019"));
+        earthquakes.add(new Earthquake("5.1","Manchester","13/6/2015"));
+        earthquakes.add(new Earthquake("4.2","Westlands","13/4/2019"));
+        earthquakes.add(new Earthquake("7.3","Mombasa","13/4/2019"));
+        earthquakes.add(new Earthquake("2.0","Delhi","13/4/2019"));
+        earthquakes.add(new Earthquake("5.6","Dubai","13/4/2019"));
+        earthquakes.add(new Earthquake("2.3","Rongai","13/4/2019"));
+        earthquakes.add(new Earthquake("6.6","Nanyuki","13/4/2019"));
+        earthquakes.add(new Earthquake("9.9","New York","13/4/2019"));
+        earthquakes.add(new Earthquake("4.7","Mlolongo","13/4/2019"));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, earthquakes);
 
-        earthquakeListView.setAdapter(adapter);
+        eAdapter=new EarthquakeAdapter(this,earthquakes);
+
+        listView.setAdapter(eAdapter);
     }
 }
